20826a94a6e2cba5b76ca3691a453facbc9f75c8
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-20 20:17:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 name = ("Josh")
 language = "Ruby"
-def introduction ( name, language )
+def introduction (name,)
   puts "Hi, my name is #{name}."
 end
 
